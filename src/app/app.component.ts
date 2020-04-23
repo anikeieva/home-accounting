9ac61b50a01e4eb29dfe3e6389a517106d8a56e6
anikeieva <alyonaoooooooo@gmail.com>
2020-04-23 23:20:39 +0300

    Init project. Added login/signin view. Added basic login logic

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'acc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  exports: [
+    ReactiveFormsModule,
+    FormsModule
+  ]
+})
+export class SharedModule { }
